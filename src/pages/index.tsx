2e1fc6b1fde9653a6bf10a6720bff42b48563944
s1f102201391 <107389125+s1f102201391@users.noreply.github.com>
2024-06-21 23:48:48 +0900

    数字表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ const Home = () => {
   // 9 -> 石とはてな
   // 10 -> 石と旗
   // 11 -> ボムセル
-  const board: number[][] = [
+  const [board, setBoard] = useState([
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -55,7 +55,7 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-  ];
+  ]);
 
   const directions = [
     [0, 1], //下
@@ -70,6 +70,46 @@ const Home = () => {
 
   //右クリック
 
+  //81マス全部の8方向の爆弾数count
+  let count = 0;
+  // const aroundBomb = () => {
+  //   // const a = 1;
+  //   for (let d = 0; d < 9; d++) {
+  //     for (let c = 0; c < 9; c++) {
+  //       if (bombMap[c][d] !== 1) {
+  //         for (const item of directions) {
+  //           const [a, b] = item;
+  //           const X = d + a;
+  //           const Y = c + b;
+  //           if (bombMap[Y] !== undefined && bombMap[Y][X] !== undefined) {
+  //             if (bombMap[Y][X] === 1) {
+  //               count++;
+  //               if (userInputs[c][d] === 1) {
+  //                 if (bombMap[c][d] !== 1) {
+  //                   if (count > 0) {
+  //                     newBoard[c][d] = count;
+  //                     // return (
+  //                     //   <div className={styles.reset} style={{ backgroundPosition: `${-30 * n}px 0` }} />
+  //                     // );
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //         if (count > 0) {
+  //           count = 0;
+  //         }
+  //       }
+  //     }
+  //     // console.log(a);
+  //   }
+  //   // setBombMap(newBombMap);
+  //   // setUserInputs(newUserInputs);
+  //   // setBoard(newBoard);
+  // };
+  // console.log(count);
+
   //ランダム取得
   function getRandomInt(min: number, max: number) {
     // min = Math.ceil(min);
@@ -79,59 +119,106 @@ const Home = () => {
   // クリックしたとき
   const newBombMap = structuredClone(bombMap);
   const newUserInputs = structuredClone(userInputs);
+  const newBoard = structuredClone(board);
 
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     if (bombMap[y][x] === 0) {
-      newBombMap[y][x] = 2;
+      newUserInputs[y][x] = 1;
       const inputfilter = (col: number) => newBombMap.flat().filter((v) => v === col).length;
 
       //初回クリック時爆弾設置
-      if (inputfilter(2) === 1) {
+      if (inputfilter(1) === 0) {
         while (inputfilter(1) < 10) {
           const nx = getRandomInt(0, 8);
           const ny = getRandomInt(0, 8);
           newBombMap[ny][nx] = 1;
-          console.log(newBombMap[ny][nx]);
-          if (newBombMap[ny][nx] === 1) {
-            board[ny][nx] = 11;
+          // console.log(newBombMap[ny][nx]);
+          if (newBombMap[ny][nx] === newBombMap[y][x]) {
+            return;
           }
+          // if (newBombMap[ny][nx] === 1) {
+          //   board[ny][nx] = 11;
+          // }
         }
-        newBombMap[y][x] = 0;
-        board[y][x] = 0;
       }
       console.log(inputfilter(1));
     }
-
-    setBombMap(newBombMap);
-    console.table(newBombMap);
-    console.table(board);
-  };
-  //81マス全部の8方向の爆弾数n
-  let n = 0;
-  const aroundBomb = () => {
-    for (let d = 0; d < 9; d++) {
-      for (let c = 0; c < 9; c++) {
-        if (newBombMap[c][d] !== 1) {
-          for (const item of directions) {
-            const [a, b] = item;
-            const X = d + a;
-            const Y = c + b;
-
-            if (
-              newBombMap[Y] !== undefined &&
-              newBombMap[Y][X] !== undefined &&
-              newBombMap[Y][X] === 1
-            ) {
-              n += 1;
-              console.log(n);
+    if (userInputs[y][x] === 0) {
+      newUserInputs[y][x] = 1;
+    }
+    // const changeBoard = (x: number, y: number) => {
+    if (newUserInputs[y][x] === 1) {
+      if (newBombMap[y][x] === 0) {
+        newBoard[y][x] = 0;
+        // const aroundBomb = () => {
+        // const a = 1;
+        for (let d = 0; d < 9; d++) {
+          for (let c = 0; c < 9; c++) {
+            if (newBombMap[c][d] !== 1) {
+              for (const item of directions) {
+                const [a, b] = item;
+                const X = d + a;
+                const Y = c + b;
+                if (newBombMap[Y] !== undefined && newBombMap[Y][X] !== undefined) {
+                  if (newBombMap[Y][X] === 1) {
+                    count++;
+                    if (newUserInputs[c][d] === 1) {
+                      if (newBombMap[c][d] !== 1) {
+                        if (count > 0) {
+                          newBoard[c][d] = count;
+                          // return (
+                          //   // <div
+                          //   //   className={styles.reset}
+                          //   //   style={{ backgroundPosition: `${-30 * count}px 0` }}
+                          //   // />
+                          // );
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            if (count > 0) {
+              count = 0;
             }
           }
+          // console.log(a);
+          // }
+          // setBombMap(newBombMap);
+          // setUserInputs(newUserInputs);
+          // setBoard(newBoard);
         }
       }
     }
+    // };s
+    // changeBoard;
+    // aroundBomb();
+    // aroundBomb();
+    setBombMap(newBombMap);
+    setUserInputs(newUserInputs);
+    setBoard(newBoard);
   };
-  aroundBomb;
+
+  // board表示
+  // const changeBoard = (x: number, y: number) => {
+  //   if (userInputs[y][x] === 1) {
+  //     if (bombMap[y][x] === 0) {
+  //       newBoard[y][x] = 0;
+  //     }
+  //   }
+  //   if (newUserInputs[y][x] === 1) {
+  //     if (newBombMap[y][x] !== 1 && n > 0) {
+  //       board[y][x] = n;
+  //     }
+  //   }
+  // };
+  // changeBoard;
+
+  // console.table(userInputs);
+  console.table(bombMap);
+  console.table(newBoard);
 
   return (
     <div className={styles.container}>
@@ -163,15 +250,15 @@ const Home = () => {
                     onClick={() => clickHandler(x, y)}
                     style={{
                       backgroundColor: bomb === -1 ? '#00ff1a' : '#bbb',
-                      ...(bomb === 0 && { backgroundPosition: `${-30 * n}px 0` }),
+                      // ...(bomb === 0 && { backgroundPosition: `${-30 * n}px 0` }),
                     }}
                   >
-                    {/* {bomb !== 0 && (
+                    {bomb !== -1 && bomb !== 0 && (
                       <div
                         className={styles.reset}
-                        style={{ backgroundPosition: `${-30 * n}px 0` }}
+                        style={{ backgroundPosition: `${-30 * (board[y][x] - 1)}px 0` }}
                       />
-                    )} */}
+                    )}
                   </div>
                 )),
               )}
